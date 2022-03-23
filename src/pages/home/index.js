eafcc17ddd77842dbb088ff298e1a19e1267f16f
Muhammad Vikri <muhammadvikri@MacBook-Pro-Muhammad.local>
2022-03-23 18:16:37 +0700

    Giphy GIF

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function Index() {
+  return (
+    <div className="container">
+      <h1>Hayuk</h1>
+      <input className="search" placeholder="Search" />
+      <button className="button">Go</button>
+      <img
+        src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
+        alt="apaajadeh"
+      />
+    </div>
+  );
+}
